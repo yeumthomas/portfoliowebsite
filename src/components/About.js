6672ffb56936168474211d9f94eb51e2f0466c45
Thomas Yeum <yeum.thomas@gmail.com>
2020-02-02 00:00:29 -0600

    added experiences tab

--- src/components/About.js
+++ src/components/About.js
@@ -12,8 +12,7 @@ class About extends Component {
                         <img className="about-picture" src="propic.jpg" alt="My Profile Picture"></img>
                     </div>
                     <div className="about-bio">
-                        <h6 className="bio-header">Hi, I'm Thomas, a computer science</h6>
-                        <p1 className="bio-body">major at <a className="school-link" target="_blank" href="https://www.rice.edu/">Rice University</a> in Houston, TX 
+                        <p1 className="bio-body">Hi, I'm Thomas, a computer science major at <a className="school-link" target="_blank" href="https://www.rice.edu/">Rice University</a> in Houston, TX 
                             with an interest in many applications but mostly in web development. I came to love building 
                             websites after discovering its connection to another passion of mine: 
                             composing classical music (samples are below!). <br/><br/>All in all, 
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,10 +6,40 @@ class Experience extends Component {
         return (
         
         <div className="experience-section" id="experience">
-            <p1>coming soon . . .</p1>
-        
-        </div>
-        );
+            <h1 className="experience-title">2. Featured Experiences  ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬</h1>
+            <div className="experience12">
+                <div className="experience1-left">
+                    <div className="experience1-title">
+                        <div className="experience1-titleleft">< h3 className="experience1-title-pt1">Project: </h3></div> 
+                        <div className="experience1-titleright">< h3 className="experience1-title-pt2">Houston Deserted</h3></div>
+                    </div>
+                    <h6 className="experience1-subtitle">2nd Place Overall Winner @ Rice Datathon, Jan 2020</h6>
+                    <ul className="experience1-textbox">
+                        <li className="experience1-text">Analyzed existing data on food deserts in Houston to predict the density at which food deserts would exist in other sections of the surrounding Houston area.</li>
+                        <li className="experience1-text">Programmed a website to organize data and aid in user visualzation of data.</li>
+                    </ul>
+                    <div className="exp1-holder">
+                    <a target="_blank" className="experience1-piclink" href="https://www.houstondeserted.netlify.com"><img className="experience1-pic" src="houstondeserted.png" alt="Houston Deserted" /></a>
+                    </div>
+                    <div className="technologies-holder"><p className="technologies">Python Jupyter Excel KML HTML CSS</p></div>
+                </div>
+                <div className="experience2-right">
+                    <div className="experience1-title">
+                        <div className="experience2-titleleft">< h3 className="experience2-title-pt1">Externship: </h3></div> 
+                        <div className="experience2-titleright">< h3 className="experience2-title-pt2">Google</h3></div>
+                    </div>
+                    <div>
+                    <h6 className="experience2-subtitle">Experienced Google, Jan 2020</h6>
+                    <ul className="experience1-textbox">
+                        <li className="experience1-text">Conducted informational interviews with Stadia and Google Assistant UX designers, back-end SWE, and Google Maps and Google Nest developers to understand behind-the-scenes work.</li>
+                        <li className="experience1-text">Introduced to React.JS framework and jumpstarted my track into web development.</li>
+                        <li className="experience1-text">Gained an eye towards what Google sees in its employees, giving me another perspective at the what skills matter.</li>
+                    </ul>
+                    <div className="technologies-holder-google"><p className="technologies">React, HTML, CSS, JS</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>);
     }
 }
 
